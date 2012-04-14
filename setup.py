dda2fc5d83aeebaeec6136207f00e08156ec0f52
C Nelson <cnelsonsic@gmail.com>
2012-04-14 01:52:33 -0500

    Add a MANIFEST and setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+from distutils.core import setup
+
+setup(name='prysenter',
+      version='1.0',
+      author='Charles Nelson',
+      author_email='cnelsonsic@gmail.com',
+      packages=['prysenter'],
+      url='http://git.io/prysenter',
+      license='LICENSE.txt',
+      description='A minimal, text-only, terminal-only presentation runner.',
+      long_description=open('README.md').read(),
+      )
